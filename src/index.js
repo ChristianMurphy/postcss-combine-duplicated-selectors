d95566205eab28823eddb8e7422d797520f27fdb
Christian Murphy <christian.murphy.42@gmail.com>
2016-05-22 13:03:05 -0700

    handle different code styles
    transform css into a consistent format in symbol table keys

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
 import postcss from 'postcss';
+import parser from 'postcss-selector-parser';
+
+const uniformStyle = parser(selector => {
+  selector.walk(node => {
+    if (node.type === 'combinator') {
+      node.value = node.value.trim().concat(' ');
+      node.spaces = {};
+    }
+  });
+});
 
 export default postcss.plugin('postcss-combine-duplicated-selectors', () => {
   return css => {
     const symbolTable = new Map();
 
     css.walkRules(rule => {
-      if (symbolTable.has(rule.selector)) {
+      const selector = uniformStyle.process(rule.selector).result;
+      if (symbolTable.has(selector)) {
         // move declarations to original rule
-        rule.nodes.forEach(decl => decl.moveTo(symbolTable.get(rule.selector)));
+        rule.nodes.forEach(decl => decl.moveTo(symbolTable.get(selector)));
         // remove duplicated rule
         rule.remove();
       } else {
         // add new selector to symbol table
-        symbolTable.set(rule.selector, rule);
+        symbolTable.set(selector, rule);
       }
     });
   };
