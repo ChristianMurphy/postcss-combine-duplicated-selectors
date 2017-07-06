498d45fae57ce5e6e3950fac27fea355511587d1
Badwin Arevalo Vera <aldebaran798@hotmail.com>
2017-07-05 20:45:38 -0500

    Test variations added

--- test/duplicated-properties.js
+++ test/duplicated-properties.js
@@ -1,12 +1,14 @@
 const test = require('ava');
 const testFactory = require('./_test-factory');
-const plugin = require('../src')({removeDuplicatedProperties: true});
+const plugin = require('../src');
 
 /**
- * These tests remove dutplicated css properties.
+ * These tests check if duplicated properties are deleted or maintained
+ * according configuration settings.
  */
 
-const css = testFactory('css', [plugin]);
+// Duplicated properties should be removed
+let css = testFactory('css', [plugin({removeDuplicatedProperties: true})]);
 
 test(
   'remove duplicated properties when combine selectors',
@@ -18,6 +20,24 @@ test(
 test(
   'remove duplicated properties in a selector',
   css,
-  '.a {height: 10px; color: black; background: orange; background: rgba(255, 165, 0, 0.5);}',
-  '.a {height: 10px; color: black; background: rgba(255, 165, 0, 0.5);}'
+  '.a {height: 10px; background: orange; background: rgba(255, 165, 0, 0.5);}',
+  '.a {height: 10px; background: rgba(255, 165, 0, 0.5);}'
 );
+
+// Duplicated properties should be maintained
+css = testFactory('css', [plugin({removeDuplicatedProperties: false})]);
+
+test(
+  'maintain duplicated properties when combine selectors',
+  css,
+  '.a {height: 10px; color: black;} .a {color: blue; width: 20px;}',
+  '.a {height: 10px; color: black; color: blue; width: 20px;}'
+);
+
+test(
+  'maintain duplicated properties in a selector',
+  css,
+  '.a {height: 10px; background: orange; background: rgba(255, 165, 0, 0.5);}',
+  '.a {height: 10px; background: orange; background: rgba(255, 165, 0, 0.5);}'
+);
+
