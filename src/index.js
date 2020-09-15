1cd93a621020eaef1c5b703d09079c96a3acfca6
Christian Murphy <christian.murphy.42@gmail.com>
2020-09-15 14:10:37 -0700

    refactor!: migrate to postcss 8 plugin format
    based off https://evilmartians.com/chronicles/postcss-8-plugin-migration

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-const postcss = require('postcss');
 const parser = require('postcss-selector-parser');
 const {name} = require('../package.json');
 
@@ -67,64 +66,67 @@ const defaultOptions = {
   removeDuplicatedProperties: false,
 };
 
-module.exports = postcss.plugin(name, (options) => {
+module.exports = (options) => {
   options = Object.assign({}, defaultOptions, options);
-  return (css) => {
-    // Create a map to store maps
-    const mapTable = new Map();
-    // root map to store root selectors
-    mapTable.set('root', new Map());
+  return {
+    postcssPlugin: name,
+    Root: (css) => {
+      // Create a map to store maps
+      const mapTable = new Map();
+      // root map to store root selectors
+      mapTable.set('root', new Map());
 
-    css.walkRules((rule) => {
-      let map;
-      // Check selector parent for any at rule
-      if (rule.parent.type === 'atrule') {
-        // Use name and query params as the key
-        const query =
-          rule.parent.name.toLowerCase() +
-          rule.parent.params.replace(/\s+/g, '');
+      css.walkRules((rule) => {
+        let map;
+        // Check selector parent for any at rule
+        if (rule.parent.type === 'atrule') {
+          // Use name and query params as the key
+          const query =
+            rule.parent.name.toLowerCase() +
+            rule.parent.params.replace(/\s+/g, '');
 
-        // See if this query key is already in the map table
-        map = mapTable.has(query) ? // If it is use it
-          mapTable.get(query) : // if not set it and get it
-          mapTable.set(query, new Map()).get(query);
-      } else {
-        // Otherwise we are dealing with a selector in the root
-        map = mapTable.get('root');
-      }
+          // See if this query key is already in the map table
+          map = mapTable.has(query) ? // If it is use it
+            mapTable.get(query) : // if not set it and get it
+            mapTable.set(query, new Map()).get(query);
+        } else {
+          // Otherwise we are dealing with a selector in the root
+          map = mapTable.get('root');
+        }
 
-      const selector = uniformStyle.processSync(rule.selector, {
-        lossless: false,
+        const selector = uniformStyle.processSync(rule.selector, {
+          lossless: false,
+        });
+
+        if (map.has(selector)) {
+          // store original rule as destination
+          const destination = map.get(selector);
+          // move declarations to original rule
+          while (rule.nodes.length > 0) {
+            destination.append(rule.nodes[0]);
+          }
+          // remove duplicated rule
+          rule.remove();
+
+          if (
+            options.removeDuplicatedProperties ||
+            options.removeDuplicatedValues
+          ) {
+            removeDupProperties(destination, options.removeDuplicatedValues);
+          }
+        } else {
+          if (
+            options.removeDuplicatedProperties ||
+            options.removeDuplicatedValues
+          ) {
+            removeDupProperties(rule, options.removeDuplicatedValues);
+          }
+          // add new selector to symbol table
+          map.set(selector, rule);
+        }
       });
-
-      if (map.has(selector)) {
-        // store original rule as destination
-        const destination = map.get(selector);
-        // move declarations to original rule
-        while (rule.nodes.length > 0) {
-          destination.append(rule.nodes[0]);
-        }
-        // remove duplicated rule
-        rule.remove();
-
-        if (options.removeDuplicatedProperties ||
-          options.removeDuplicatedValues) {
-          removeDupProperties(
-              destination,
-              options.removeDuplicatedValues,
-          );
-        }
-      } else {
-        if (options.removeDuplicatedProperties ||
-          options.removeDuplicatedValues) {
-          removeDupProperties(
-              rule,
-              options.removeDuplicatedValues,
-          );
-        }
-        // add new selector to symbol table
-        map.set(selector, rule);
-      }
-    });
+    },
   };
-});
+};
+
+module.exports.postcss = true;
